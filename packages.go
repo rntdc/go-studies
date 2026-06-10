ed7da14d7d757268c6ca5d1d08dc400ebb89febf
Renato Dekeches <renato.dekeches@Renatos-MacBook-Air.local>
2026-06-10 16:50:09 -0300

    packages basics

---
+++ packages.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now we have %g problems.\n", math.Sqrt(7))
+}
