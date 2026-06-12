810e2d78e23e94a81a864489b31ee17d5464d0a8
Renato Dekeches <renato.dekeches@Renatos-MacBook-Air.local>
2026-06-11 23:43:51 -0300

    function and instant function

--- packages.go
+++ packages.go
@@ -7,6 +7,21 @@ import (
 )
 
 func main() {
+	var first int = 10
+	var second int = 20
+
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Printf("Now we have %g problems.\n", math.Sqrt(7))
+
+	fmt.Println("Add function:", add(first, second))
+
+	resultado := func(a, b int) int {
+		return a + b
+	}(first, 1)
+
+	fmt.Println("Instant function: ", resultado)
+}
+
+func add(x int, y int) int {
+	return x + y
 }
